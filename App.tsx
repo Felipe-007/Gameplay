e1da6687a6cfed72a92591f07b8738153ab52a88
Felipe <felipe.presidencia@gmail.com>
2022-01-16 21:56:08 -0300

    C3 - handleSignIn, avatar

--- App.tsx
+++ App.tsx
@@ -7,8 +7,8 @@ import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhan
 import AppLoading from "expo-app-loading";
 import { useFonts } from 'expo-font';
 
-import { SignIn } from './src/screens/SignIn';
-import { Background } from './src/components/Background';
+import { Routes } from './src/routes';
+import { Background } from "./src/components/Background";
 
 export default function App() {
   const [fontsLoaded] = useFonts({  //cria a const fontsLoaded
@@ -29,7 +29,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <SignIn />
+      <Routes />
     </Background>
   );
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -4,17 +4,24 @@ import React from "react";
 import {
     View,
     Text,
-    Image,    
+    Image,
 } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-import { ButtonIcon } from "../../components/ButtonIcon";
 import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles';
 
+import { ButtonIcon } from "../../components/ButtonIcon";
+
 export function SignIn() {
+    const navigation = useNavigation();
+
+    function handleSignIn() {
+        navigation.navigate('Home');
+    }
+
     return (
         <View style={styles.container}>
-            
             <Image
                 source={IllustrationImg}
                 style={styles.image}
@@ -34,8 +41,8 @@ export function SignIn() {
                 </Text>
 
                 <ButtonIcon
-                    title="Entrar com Discord"
-                    activeOpacity={0.7}
+                    title="Entrar com Discord"                    
+                    onPress={handleSignIn}
                 />
 
             </View>
