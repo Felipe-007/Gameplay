0747c9933528c3183f7734978d03db107ad50adc
Felipe <felipe.presidencia@gmail.com>
2022-01-30 22:04:49 -0300

    C11 - Member, AppointmentDetails

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from "react";
 import { LinearGradient } from "expo-linear-gradient";
 import { BorderlessButton } from "react-native-gesture-handler";
+import { useNavigation } from '@react-navigation/native';
 import { Text, View } from "react-native";
 import { theme } from '../../global/styles/theme';
 import { styles } from "./styles";
@@ -14,12 +15,18 @@ type Props = {
 export function Header({ title, action }: Props) {
     const { secondary100, secondary40, heading } = theme.colors;
 
+    const navigation = useNavigation();  // ação de voltar
+
+    function handleGoBack(){  // ação de voltar
+        navigation.goBack();
+    }
+
     return (
         <LinearGradient
             style={styles.container}
             colors={[secondary100, secondary40]}
         >
-            <BorderlessButton>
+            <BorderlessButton onPress={handleGoBack}>
                 <Feather
                     name="arrow-left"
                     size={24}
@@ -32,9 +39,9 @@ export function Header({ title, action }: Props) {
             </Text>
 
             {
-                action &&
+                action &&  //caso tenho o botão para compartilhamento, será mostrado
                 <View>
-                    { }
+                    { action }
                 </View>
             }
 
